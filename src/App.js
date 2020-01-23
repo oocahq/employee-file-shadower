334185b574058845f9d2302754da0bdcfffed954
Thanatcha Sangphet <thanatcha.s@ooca.co>
2020-01-24 01:21:11 +0700

    add lib and uploaded file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import ReactFileReader from 'react-file-reader'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header-text ">
-      Convert To<span className="yellow"> Shadowed</span> File
-      </div>
-      <div className="upload-bt">
-        Selected a File
-      </div>
-      <div className="convert-bt">
-        Convert
-      </div>
-    </div>
-  );
+const handleFiles = (files) => {
+  console.log('files[0]', files[0])
+  const reader = new FileReader()
+  reader.onload = function (e) {
+    console.log(reader.result)
+  }
+  reader.readAsText(files[0])
 }
 
-export default App;
+const App = () => (
+  <div className="App">
+    <div className="header-text ">
+        Convert To
+      <span className="shadowed-text"> Shadowed</span>
+      File
+    </div>
+    <ReactFileReader handleFiles={handleFiles} fileTypes={['.csv', '.xlsx']}>
+      <button className="upload-bt" type="submit">
+          Selected a File
+        <div className="upload-icon">+</div>
+      </button>
+    </ReactFileReader>
+
+    <div className="convert-bt">
+          Convert
+    </div>
+
+  </div>
+)
+export default App
