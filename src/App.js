0101dd36524da40a6c72f8ecb7e62789a4065527
Thanatcha Sangphet <thanatcha.s@ooca.co>
2020-01-24 00:52:39 +0700

    fix css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="header-text padding-top">
+      <div className="header-text ">
       Convert To<span className="yellow"> Shadowed</span> File
       </div>
-
       <div className="upload-bt">
         Selected a File
-        </div>
-        <div className="convert-bt">
-          Convert
-        </div>
+      </div>
+      <div className="convert-bt">
+        Convert
+      </div>
     </div>
   );
 }
