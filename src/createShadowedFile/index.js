772912e52a7a2dd11bdbd2b8f48cbce13c6ddf8f
Thanatcha Sangphet <thanatcha.s@ooca.co>
2020-01-25 02:53:07 +0700

    complete upload xlsx and convert to csv

--- src/createShadowedFile/index.js
+++ src/createShadowedFile/index.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
 import * as R from 'ramda'
+import XLSX from 'xlsx'
 import ReactFileReader from 'react-file-reader'
 import SHA256 from 'crypto-js/sha256'
 import './createShadowedFile.css'
+
+const FIRST_COLUMN_NAME = 'email'
+const CSV_HEADER_FILE_NAME = 'data:text/csv;charset=utf-8,'
+const EXPORTED_SHADOWED_FILE_NAME = 'shadowed-file.csv'
 
 const CreateShadowed = () => {
   const [fileName, setFileName] = useState(null)
   const [shadowedFileCsv, setShadowedFileCsv] = useState(null)
 
   const convertEmailToHash = (userData) => {
-    const result = userData.map((user) => {
-      const shadowedRow = user
-      shadowedRow[0] = SHA256(user[0]).toString()
-      return user
-    })
-    result[0][0] = 'email'
-    return result
+    if (userData.length > 0) {
+      const result = userData.map((user) => {
+        const shadowedRow = user
+        shadowedRow[0] = SHA256(user[0]).toString()
+        return user
+      })
+      result[0][0] = FIRST_COLUMN_NAME
+      return result
+    }
   }
 
 
@@ -23,13 +30,14 @@ const CreateShadowed = () => {
     const encodedUri = encodeURI(shadowedFileCsv)
     const link = document.createElement('a')
     link.setAttribute('href', encodedUri)
-    link.setAttribute('download', 'shadowedFile.csv')
+    link.setAttribute('download', EXPORTED_SHADOWED_FILE_NAME)
     document.body.appendChild(link)
     link.click()
   }
 
+
   const convertHashToCsv = (filesArray) => {
-    let csvContent = 'data:text/csv;charset=utf-8,'
+    let csvContent = CSV_HEADER_FILE_NAME
     filesArray.map((rowArray) => {
       const row = rowArray.join(',')
       csvContent += `${row}\n`
@@ -37,16 +45,38 @@ const CreateShadowed = () => {
     setShadowedFileCsv(csvContent)
   }
 
+
+  const convertCsvFileToShadowedFile = (csvUserData) => {
+    const arrayFromCsv = csvUserData.split('\n').map((ar) => ar.split(','))
+    const shadowedData = convertEmailToHash(arrayFromCsv)
+    convertHashToCsv(shadowedData)
+  }
+
   const getFileFromUpload = (files) => {
     const reader = new FileReader()
-    reader.onload = () => {
-      const arrayFromCsv = reader.result.split('\n').map((ar) => ar.split(','))
-      const shadowedData = convertEmailToHash(arrayFromCsv)
-      console.log('shadowedDAta', shadowedData)
-      convertHashToCsv(shadowedData)
+
+    // reader.onload = () => {
+    //   const arrayFromCsv = reader.result.split('\n').map((ar) => ar.split(','))
+    //   const shadowedData = convertEmailToHash(arrayFromCsv)
+    //   console.log('shadowedDAta', shadowedData)
+    //   convertHashToCsv(shadowedData)
+    //   setFileName(files[0].name)
+    // }
+    // reader.readAsText(files[0])
+
+
+    reader.onload = (event) => {
+      const data = event.target.result
+      const workBook = XLSX.read(data, { type: 'binary' })
       setFileName(files[0].name)
+      workBook.SheetNames.map((sheetName) => {
+        const csvUserData = XLSX.utils.make_csv(workBook.Sheets[sheetName])
+        convertCsvFileToShadowedFile(csvUserData)
+      })
     }
-    reader.readAsText(files[0])
+
+    reader.readAsBinaryString(files[0])
+    console.log('selectedFile', files[0])
   }
 
 
